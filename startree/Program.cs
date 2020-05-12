a8f764d697252a4513cd14dead0d263451d8b589
MoonHyekyung <moonsoft02@gmail.com>
2020-05-12 21:52:28 +0900

    for문 이용해 피라미드 별찍기

--- startree/Program.cs
+++ startree/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,20 @@ namespace startree
     {
         static void Main(string[] args)
         {
+            int length = 1;
+            for (int i = 7; i>=0; i--)
+            {
+                for(int j = i; j>=0; j--)
+                {
+                    Console.Write(' ');
+                }
+                for(int k = 1; k<=length; k++)
+                {
+                    Console.Write('*');
+                }
+                length += 2;
+                Console.WriteLine();
+            }
         }
     }
 }
